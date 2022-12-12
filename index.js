f55c208147cf534effac60365b0851c04fe8135a
N-A-V-I-D <NC1939@nyu.edu>
2022-12-11 21:03:49 -0500

    h

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ console.log('hi')
 
 $(document).ready(function() {
     $("#picture").animate({left: "+900"}, 2000);
-    fadeInName();
+    // fadeInName();
 });
 
-function fadeInName(){
-    $("#name").fadeIn(2000)
-}
+// function fadeInName(){
+//     $("#name").fadeIn(2000)
+// }
