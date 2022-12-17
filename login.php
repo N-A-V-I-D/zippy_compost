2877f6229a0c12e1e08511f34ddeda6f87d3cea4
pinkmaggs <billimargarita@gmail.com>
2022-12-17 16:00:50 -0500

    working on login php

--- login.php
+++ login.php
@@ -5,21 +5,56 @@
 </head>
 <body>
   <?php
+      // process login instruction
 
-  if(isset($_POST['clients.txt']))
-  {
-  //itirate over text in client php file
-  //check if login info can be found on get_included_file
-  //if login info can be found on get_included_file
-  //then direct to index page with full menu
-  
+      // extract infor from form
+      $user = $_POST['username'];
+      $password =$_POST['password'];
 
-  $data=$_POST['textdata'];
-  $fp = fopen('login.txt', 'a');
-  fwrite($fp, $data);
-  fclose($fp);
-  }
-  ?>
+      // open the file users.txt
+
+      $file = fopen("users.txt", "r");
+
+      $flag = FALSE;
+
+      // read one line at time from file and assign each line to a string called $line
+      while(!(feof($file)))
+      {
+
+      // get one line at time from file
+      $line= fgets($file);
+
+      // remove end of line character from line using rtrim function
+      $line = rtrim($line);
+
+      // split string on : to get each field separatly and put values in an array
+
+      $info = explode(":", $line);
+
+      // check for a match between what user inputted and whats in file
+
+      if (($user == $info[0] ) && ( $password == $info[1] ))
+      {
+      // if there is a match set flag to true and exit
+      $flag = TRUE;
+      break;
+      }
+
+      }
+
+      // check to see if there is a match flag = TRUE
+
+      if ($flag)
+      	{
+      	echo "<h2>Welcome Back" . $user . "!</h2>." . "<p>Please click on  <a href='private.html'> here </a> to visit your private site! ";;
+      	}
+
+      else
+      {
+      	echo "Can't enter site because of wrong username and password";
+      }
+
+      ?>
 
 </body>
 </html>
